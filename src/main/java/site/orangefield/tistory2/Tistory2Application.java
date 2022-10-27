dfbf98c9d8c5b467751b3d3fdffa05aa561c0bdb
field <jsw_777@naver.com>
2022-10-27 16:01:57 +0900

    Initial Commit

---
+++ src/main/java/site/orangefield/tistory2/Tistory2Application.java
@@ -0,0 +1,13 @@
+package site.orangefield.tistory2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tistory2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tistory2Application.class, args);
+	}
+
+}
